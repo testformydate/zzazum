981ba5dbac6b3c87839b050e4a6fcfd615c3b061
LeeSeoYeon <sps1122@naver.com>
2016-02-03 18:08:34 +0900

    2016/02/03 18:07분 3 강의실 전.수.민. create view table 만듬. postscript > repository > postscriptDao 적용 쿼리문 변경했다고 서연이가 시킴.

--- src/main/java/com/mydate/zzazum/postscript/repository/PostScriptDao.java
+++ src/main/java/com/mydate/zzazum/postscript/repository/PostScriptDao.java
@@ -11,19 +11,23 @@ import com.mydate.zzazum.postscript.vo.PostScriptList;
 @Repository
 public interface PostScriptDao {
 	
-	@Select("select ps.*,  mem_primg  from md_postscript as ps inner join md_member on mem_id = ps_email order by ps_date desc, ps_no desc limit 0,7")
+
+	@Select("select * from vmd_pslistall limit 0,7")
 	public ArrayList<PostScriptList> psListAll();
 	
 	@Select("select count(*) from md_postscript")
 	public String psListCnt();
 	
-	@Select("select ps.*,  mem_primg  from md_postscript as ps inner join md_member on mem_id = ps_email order by ps_date desc, ps_no desc limit #{track_Num},#{track_Count}")
+
+	@Select("select * from vmd_pslistpart limit #{track_Num},#{track_Count}")
 	public ArrayList<PostScriptList> psListPart(PostScriptList bean);
 	
-	@Select("select distinct mem_primg from md_postscript inner join md_member on mem_id = ps_email order by ps_hits desc limit 0,4")
+
+	@Select("select * from vmd_psbestplanner")
 	public ArrayList<MemberVo> psBestPlanner();
 	
-	@Select("select ps.*, mem_primg from md_postscript as ps inner join md_member on mem_id = ps_email where ps_like in(select max(ps_like) from md_postscript group by ps_location) order by ps_location asc;")
+	
+	@Select("select * from vmd_psbest")
 	public ArrayList<PostScriptList> psBest();
 	
 }
